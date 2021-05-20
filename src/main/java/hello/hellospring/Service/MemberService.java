210e41dbbf0fcd4bfed327784e0e5504b1573c5a
hyechan <dhgpcks@gmail.com>
2021-05-20 20:33:11 +0900

    회원 서비스 개발

---
+++ src/main/java/hello/hellospring/Service/MemberService.java
@@ -0,0 +1,49 @@
+package hello.hellospring.Service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * member로 회원가입
+     * @param member
+     * @return
+     */
+    public Long join(Member member){
+        //같은 이름이 있는 경우 회원x
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원이다");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     * @return
+     */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    /**
+     * memberId로 회원 조회
+     * @param memberId
+     * @return
+     */
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
